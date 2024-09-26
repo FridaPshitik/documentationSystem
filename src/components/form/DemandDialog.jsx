058aebfe59569af72456e74908486d305c2c3439
YaffaWolkowich <yafiv@skyvar.co.il>
2024-09-26 13:27:22 +0300

    :mailbox_with_mail: Edit the email in demond dialog

--- src/components/form/DemandDialog.jsx
+++ src/components/form/DemandDialog.jsx
@@ -9,7 +9,7 @@ export default function DemandDialog(data) {
             <Card title={"יצירת קשר"}  >
             <h3> איש הקשר: {data.dataSystem.contactName}</h3>
             <h3> מספר הטלפון: {data.dataSystem.contactPhone}</h3>
-            <h3> מייל: {data.dataSystem.contactEmail}</h3>
+            <h3> מייל: <a href={'mailto:'+data.dataSystem.contactEmail}>{data.dataSystem.contactEmail}</a></h3>
             </Card>
         </div>
     )
