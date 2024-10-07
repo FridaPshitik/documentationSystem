3993ee4a57bbea52c562b30f012dd980b8a6ad4c
esterhamburger <Etih@skyvar.co.il>
2024-10-07 15:09:05 +0300

    :speech_balloon: Adding server error display

--- src/App.js
+++ src/App.js
@@ -6,25 +6,28 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import SystemsTable from './components/table/SystemsTable';
 import { ProjectContext } from './services/ProjectContext';
 import { useEffect, useState } from 'react';
-import { getExternalFactor } from './services/ProjectService';
+import { getExternalFactor, getInternalFactor } from './services/ProjectService';
 import { getInternalDisplay } from './services/InternalService';
 
 function App() {
   const [projects, setProjects] = useState([]);
   const [externals, setExternals] = useState([]);
   const [internals, setInternals] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      setExternals(await getExternalFactor());
-      setInternals(await getInternalDisplay());
+      let externals = await getExternalFactor();
+      let internals = await getInternalFactor();
+      externals.status === 200 ? setExternals(externals.data) : setError(externals.message + ' -- שגיאה בקבלת גופים חיצוניים');
+      internals.status === 200 ? setInternals(internals.data) : setError(internals.message + ' --שגיאה בקבלת גופים פנימיים');
     }
     fetchData();
   }, []);
 
-  
+
   return <>
-    <ProjectContext.Provider value={{ projects, setProjects, externals, setExternals, internals, setInternals }}>
+    <ProjectContext.Provider value={{ projects, setProjects, externals, setExternals, internals, setInternals, error, setError }}>
       <SystemsTable></SystemsTable>
     </ProjectContext.Provider>
   </>
--- src/services/axiosInstance.jsx
+++ src/services/axiosInstance.jsx
@@ -3,22 +3,42 @@ import axios from "axios";
 const serverPath = process.env.REACT_APP_SERVER_PATH || "http://localhost:8000"
 
 
-export const get = async (tablePath) =>{ 
-    const answer = await axios.get(`${serverPath}/${tablePath}`)
-    return answer.data
+export const get = async (tablePath) => {
+    try {
+        const answer = await axios.get(`${serverPath}/${tablePath}`)
+        return answer;
+    }
+    catch (error) {
+        return error;
+    }
 }
 
-export const post = async (tablePath, data) =>{
-    const answer = await axios.post(`${serverPath}/${tablePath}`, data)
-    return answer.data
+export const post = async (tablePath, data) => {
+    try {
+        const answer = await axios.post(`${serverPath}/${tablePath}`, data)
+        return answer;
+    }
+    catch (error) {
+        return error;
+    }
 }
 
-export const del = async (tablePath, id) =>{      
-    const answer = await axios.delete(`${serverPath}/${tablePath}/${id}`)
-    return answer.data
+export const del = async (tablePath, id) => {
+    try {
+        const answer = await axios.delete(`${serverPath}/${tablePath}/${id}`)
+        return answer;
+    }
+    catch (error) {
+        return error;
+    }
 }
 
-export const put = async (tablePath, data) =>{      
-    const answer = await axios.put(`${serverPath}/${tablePath}`, data)
-    return answer.data
+export const put = async (tablePath, data) => {
+    try {
+        const answer = await axios.put(`${serverPath}/${tablePath}`, data)
+        return answer;
+    }
+    catch (error) {
+        return error;
+    }
 }
