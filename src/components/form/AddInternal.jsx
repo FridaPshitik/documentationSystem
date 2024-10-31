351458ac7b0060a98efc6fa5c8592a34a5d01770
pninaPolchak <pninaP@skyvar.co.il>
2024-10-31 15:00:16 +0200

    🔐 Add validation

--- src/components/form/AddInternal.jsx
+++ src/components/form/AddInternal.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { FloatLabel } from 'primereact/floatlabel';
 import { InputText } from 'primereact/inputtext';
@@ -11,85 +12,108 @@ import { displayToast } from '../../services/toast';
 
 import './Form.css';
 
-export const AddInternal = ({ setProject, hide ,parent , toast}) => {
-  const {internals, setInternals} = useContext(ProjectContext);
-  const [internal, setInternal]=useState({name:'',command:'',contact:'',department:'',phone:'',email:''});
-  const [formSubmitted, setFormSubmitted] = useState(false);
+export const AddInternal = ({ setProject, hide, parent, toast }) => {
+  const { internals, setInternals } = useContext(ProjectContext);
 
-  const dataValidation = (data) => { 
-    return data.name &&
-      data.command &&
-      data.contact &&
-      data.phone &&
-      data.email;
-  };
-    
-  const submit = async (event) => {
-    event.preventDefault();
+  let {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
 
-    if (dataValidation(internal)) {
-      const res = await createInternal(internal);
-      let updates;
-      
-      if(res.data){
-        const ans = res.data;
-        await setInternals([ ...internals.slice(0, internals.length - 1), ans, ...internals.slice(internals.length - 1)]);
-        updates = (parent==='require'?  {require : ans ,requiresId: ans.id} :  {internal : ans ,internalId: ans.id});
-        displayToast(toast, 'success', 'Success', ans.name+' נוסף בהצלחה');
-      }
 
-      else if(res.response.data.error){
-        updates = (parent==='require'?  {require : null ,requiresId: null} :  {internal : null ,internalId: null});
-        displayToast(toast, 'error', 'Error', res.response.data.error);
-      }
+  const submit = async (data) => {
+    const res = await createInternal(data);
+    let updates;
 
-      setProject((prevProject) => ({ ...prevProject, ...updates}));
-      hide(false);
+    if (res.data) {
+      const ans = res.data;
+      await setInternals([...internals.slice(0, internals.length - 1), ans, ...internals.slice(internals.length - 1)]);
+      updates = (parent === 'require' ? { require: ans, requiresId: ans.id } : { internal: ans, internalId: ans.id });
+      displayToast(toast, 'success', 'Success', ans.name + ' נוסף בהצלחה');
     }
-    else{
-      setFormSubmitted(true);
+
+    else if (res.response.data.error) {
+      updates = (parent === 'require' ? { require: null, requiresId: null } : { internal: null, internalId: null });
+      displayToast(toast, 'error', 'Error', res.response.data.error);
     }
+
+    setProject((prevProject) => ({ ...prevProject, ...updates }));
+    hide(false);
   };
-    
 
   return (<>
-    <form action='' onSubmit={submit}>
+    <form action='' onSubmit={handleSubmit(submit)}>
       <div>
+
         <FloatLabel className='field'>
-          <InputText className='input md:w-14rem field' inputid='name' value={internal.name} onChange={(e) => setInternal((prevInternal) => ({...prevInternal, name: e.target.value}))} />
+          <InputText id='name' aria-describedby="name-help" className='input md:w-14rem field'
+            type='name'
+            {...register('name', {
+              required: { value: true, message: '  שם יחידה הינו שדה חובה  ' },
+            })} />
           <label htmlFor='name'>שם יחידה</label>
         </FloatLabel>
-        {formSubmitted && !internal.name && (
-          <Message severity='error' text='שדה זה הינו חובה' />
+        {errors.name && (
+          <Message id="name-help" severity='error' text={errors.name.message} />
         )}
+
         <FloatLabel className='field'>
-          <InputText className='input md:w-14rem field' inputid='command' value={internal.command} onChange={(e) => setInternal((prevInternal) => ({...prevInternal, command: e.target.value}))} />
+          <InputText id='command' aria-describedby="command-help" className='input md:w-14rem field'
+            type='command'
+            {...register('command', {
+              required: { value: true, message: 'אזור פיקוד הינו שדה חובה' },
+            })} />
           <label htmlFor='command'>אזור פיקוד</label>
         </FloatLabel>
-        {formSubmitted && !internal.command && (
-          <Message severity='error' text='שדה זה הינו חובה' />
+        {errors.command && (
+          <Message id="command-help" severity='error' text={errors.command.message} />
         )}
+
         <FloatLabel className='field'>
-          <InputText className='input md:w-14rem field' inputid='contact' value={internal.contact} onChange={(e) => setInternal((prevInternal) => ({...prevInternal, contact: e.target.value}))} />
+          <InputText id='contact' aria-describedby="contact-help" className='input md:w-14rem field'
+            type='contact'
+            {...register('contact', {
+              required: { value: true, message: '  שם איש קשר הינו שדה חובה  ' },
+            })} />
           <label htmlFor='contact'>שם איש קשר</label>
         </FloatLabel>
-        {formSubmitted && !internal.contact && (
-          <Message severity='error' text='שדה זה הינו חובה' />
+        {errors.contact && (
+          <Message id="contact-help" severity='error' text={errors.contact.message} />
         )}
+
         <FloatLabel className='field'>
-          <InputText className='input md:w-14rem field' inputid='phone' value={internal.phone} onChange={(e) => setInternal((prevInternal) => ({...prevInternal, phone: e.target.value}))} />
+          <InputText id='phone' aria-describedby="phone-help" className='input md:w-14rem field'
+            type='phone'
+            {...register('phone', {
+              required: { value: true, message: '  טלפון הינו שדה חובה  ' },
+              type: { String: true },
+              phone: { value: true, message: 'טלפון לא חוקי ' },
+              pattern: {
+                value: /^[0-9]{10}$/,
+                message: ' טלפון לא חוקי '
+              }
+            })} />
           <label htmlFor='phone'>טלפון איש קשר</label>
         </FloatLabel>
-        {formSubmitted && !internal.phone && (
-          <Message severity='error' text='שדה זה הינו חובה' />
+        {errors.phone && (
+          <Message id="phone-help" severity='error' text={errors.phone.message} />
         )}
+
+
         <FloatLabel className='field'>
-          <InputText className='input md:w-14rem field' inputid='email' value={internal.email} onChange={(e) => setInternal((prevInternal) => ({...prevInternal, email: e.target.value}))} />
+          <InputText id='email' aria-describedby="email-help" className='input md:w-14rem field'
+            type='email'
+            {...register('email', {
+              required: { value: true, message: '  אימייל הינו  חובה  ' },
+              email: { value: true, message: ' כתובת אימייל לא חוקית ' },
+            })} />
           <label htmlFor='email'>מייל איש קשר</label>
         </FloatLabel>
-        {formSubmitted && !internal.email && (
-          <Message severity='error' text='שדה זה הינו חובה' />
+        {errors.email && (
+          <Message id="email-help" severity='error' text={errors.email.message} />
         )}
+
       </div>
       <div id='button'>
         <Button severity='secondary' label='הוסף' />
