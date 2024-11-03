c19957d0e3f5996757847fdd0008617bd166da50
YaffaWolkowich <yafiv@skyvar.co.il>
2024-11-03 12:52:52 +0200

    sort by name and add interval refresh

--- src/components/table/SystemsTable.jsx
+++ src/components/table/SystemsTable.jsx
@@ -245,6 +245,7 @@ export default function SystemsTable() {
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log('hhhhhhhhhhhh');
       let getProject = await getProjects();
       let externals = await getExternals();
       let internals = await getInternals();
@@ -264,7 +265,8 @@ export default function SystemsTable() {
       } 
     };
     fetchData();
-  }, []);
+    const myInterval = setInterval(fetchData, 1000 * 60 * 24 * process.env.REACT_APP_REFRESH_TIME);
+    return () => clearInterval(myInterval);  }, []);
 
   const addPerform = (obj) => {
     if (obj.internal)
@@ -403,8 +405,7 @@ export default function SystemsTable() {
             <span style={{ fontWeight: 'bold', fontSize: '2em' }}> תיעוד </span>
             <h3 id='titleH3'>תצוגת מערכות מידע</h3>
           </div>
-          <DataTable ref={dt} value={displayProjects} exportFilename='documentation' paginator editMode='row' rows={10} dataKey='id' onRowEditComplete={onRowEditComplete} onRowEditInit={onRowEditInit} filters={filters} filterDisplay='row' loading={loading} scrollable
-            selectionMode={'checkbox'} selection={selectedProjects} onSelectionChange={(e) => setSelectedProjects(e.value)}
+          <DataTable ref={dt} value={displayProjects} exportFilename="documentation" paginator editMode="row" rows={10} dataKey="id" onRowEditComplete={onRowEditComplete} onRowEditInit={onRowEditInit} sortField="name" sortOrder={1} filters={filters} filterDisplay="row" loading={loading} scrollable            selectionMode={'checkbox'} selection={selectedProjects} onSelectionChange={(e) => setSelectedProjects(e.value)}
             globalFilterFields={['name', 'purpose', 'description', 'status', 'productionTime', 'requires.command', 'factorableType', 'perform.name', 'population', 'classification', 'environment']} header={header} emptyMessage='אין מערכות להציג' >
             <Column style={{ minWidth: '5rem' }} body={openCardBodyTemplate} />
             <Column field='name' header='שם המערכת' editor={(options) => textEditor(options)} sortable filter filterPlaceholder='חפש' style={{ minWidth: '15rem' }} />
